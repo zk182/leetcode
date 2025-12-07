7bc8385772cb1671e90ac67e4820fd45221ef702
Nicolas Battaglia <nicobattmdq@gmail.com>
2025-12-07 18:11:32 -0300

    group-anagrams opt

--- js/group-anagram.js
+++ js/group-anagram.js
@@ -27,3 +27,33 @@ const groupAnagrams = function(strs) {
     }
     return r;
 };
+
+
+// optimized
+
+// eat --> 1 e, 1 a, 1 t ? false -> meto nuevo, sino meto en mismo que existe
+// ['e', 'a', 't',]
+
+// recorrer strs
+// chequear si existe hash que tenga las letras exactas
+// si? Agrego a array con mismo indice que hash
+// no? Agrego hash + palabra al array en mismo indice
+
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+const groupAnagramsOptimized = function(strs) {
+    const r = [];
+    let ans = {};
+
+    for (let s of strs) {
+        const key = s.split('').sort().join('');
+        if (!ans[key]) {
+            ans[key] = [];
+        }
+        ans[key].push(s)
+    }
+    return Object.values(ans);
+};
